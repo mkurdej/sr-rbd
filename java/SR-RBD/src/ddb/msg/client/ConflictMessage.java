e7a082f76d03f10b3ee26ca9f482c3ad92984215
marek.kurdej <marek.kurdej@752c7794-dde4-11de-849f-b362a63f070a>
2009-12-31 10:47:39 +0000

    Dodane stuby brakujących klas.

---
+++ java/SR-RBD/src/ddb/msg/client/ConflictMessage.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package ddb.msg.client;
+
+import ddb.msg.Message;
+
+/**
+ * TODO 
+ *
+ * @author Marek Kurdej (curdeius[at]gmail.com)
+ *
+ */
+public class ConflictMessage extends Message {
+
+}
