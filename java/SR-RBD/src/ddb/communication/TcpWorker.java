a098ecc3e94db206e42308f55ab377dc0876be80
xxeonic <xxeonic@752c7794-dde4-11de-849f-b362a63f070a>
2010-01-13 18:54:57 +0000

    TcpWorker - podtrzymanie połączenia

--- java/SR-RBD/src/ddb/communication/TcpWorker.java
+++ java/SR-RBD/src/ddb/communication/TcpWorker.java
@@ -19,7 +19,6 @@ public class TcpWorker implements Runnable
 {
 
     private final static String LOGGING_NAME = "TcpWorker";
-    private final static int READ_TIMEOUT = 2000;
     Socket socket;
 
     public TcpWorker(Socket newSocket)
@@ -29,42 +28,37 @@ public class TcpWorker implements Runnable
 
     public void run()
     {
-        Logger.getInstance().log("New connection from: "
-                + socket.getInetAddress().getHostAddress().toString() + ":"
-                + socket.getPort()
-                , LOGGING_NAME, Logger.Level.INFO);
-        try
-        {
-            socket.setSoTimeout(READ_TIMEOUT);
+    	Logger.getInstance().log("New connection from: "
+			+ socket.getInetAddress().getHostAddress().toString() + ":"
+			+ socket.getPort()
+			, LOGGING_NAME, Logger.Level.INFO);
+    	do
+    	{
+    		try
+    		{
+    			socket.setSoTimeout(0);
 
-            DataInputStream in = new DataInputStream(socket.getInputStream());
-            int size = in.readInt();
-            byte[] b = new byte[size];
+    			DataInputStream in = new DataInputStream(socket.getInputStream());
+    			int size = in.readInt();
+    			byte[] b = new byte[size];
 
-            Logger.getInstance().log("Size = " + Integer.toString(size),
-                    LOGGING_NAME, Logger.Level.INFO);
+    			Logger.getInstance().log("Size = " + Integer.toString(size),
+    					LOGGING_NAME, Logger.Level.INFO);
 
-            int left;
-            for(left = size; left > 0; )
-                left -= in.read(b, size - left, left);
+    			int left;
+    			for(left = size; left > 0; )
+    				left -= in.read(b, size - left, left);
 
-            DispatcherImpl.getInstance().dispatchMessage(new String(b),
-            		socket.getInetAddress().getHostAddress().toString(),
-            		socket.getPort());
-        }
-        catch (IOException ex)
-        {
-            Logger.getInstance().log("Error reading from socket!", LOGGING_NAME,
-                    Logger.Level.WARNING);
-        }
-        try
-        {
-            socket.close();
-        }
-        catch (IOException ex)
-        {
-            Logger.getInstance().log("Error closing socket!", LOGGING_NAME,
-            		Logger.Level.WARNING);
-        }
+    			DispatcherImpl.getInstance().dispatchMessage(new String(b),
+    					socket.getInetAddress().getHostAddress().toString(),
+    					socket.getPort());
+    		}
+    		catch (IOException ex)
+    		{
+    			Logger.getInstance().log("Socket exception!", LOGGING_NAME,
+    					Logger.Level.WARNING);
+    			break;
+    		}
+    	} while(true);
     }
 }
