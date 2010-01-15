39bf9e7d9d12f8648712e69c93199ae203444bce
xxeonic <xxeonic@752c7794-dde4-11de-849f-b362a63f070a>
2010-01-15 22:35:41 +0000

    Exception w dumpie i imporcie

--- trunk/java/SR-RBD/src/ddb/db/DumpTableException.java
+++ trunk/java/SR-RBD/src/ddb/db/DumpTableException.java
@@ -1,5 +1,22 @@
 package ddb.db;
 
-public class DumpTableException {
+import java.io.IOException;
 
+public class DumpTableException extends Exception 
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String errorMessage;
+	
+	public DumpTableException(IOException e)
+	{
+		errorMessage = e.getMessage();
+	}
+	
+	public String getErrorMessage()
+	{
+		return errorMessage;
+	}
 }
