cfe7ef84316c31e83e002959d21347287de29ce8
xxeonic <xxeonic@752c7794-dde4-11de-849f-b362a63f070a>
2010-01-17 14:48:17 +0000

    C# - na razie szkic

---
+++ C#/RBD/SqlConnector.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RBD
+{
+    interface SqlConnector
+    {
+        DatabaseTable query(String queryString);
+    }
+}
