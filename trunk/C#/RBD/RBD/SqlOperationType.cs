01b41046f312e7601cd55cb460680bbbff442f23
xxeonic <xxeonic@752c7794-dde4-11de-849f-b362a63f070a>
2010-01-17 18:06:01 +0000

    Dalsze problemy z katalogami

--- trunk/C#/RBD/RBD/SqlOperationType.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace RBD
-{
-    enum SqlOperationType { Undefined, Select, Insert, Update, Delete, Create, Lock, Unlock };
-}
