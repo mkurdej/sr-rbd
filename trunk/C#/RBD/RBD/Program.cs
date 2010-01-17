bdf0e8e192731ca31ea428aafba1f2cdb82f9ccc
xxeonic <xxeonic@752c7794-dde4-11de-849f-b362a63f070a>
2010-01-17 17:58:57 +0000

    Ponowne wrzucenie

---
+++ trunk/C#/RBD/RBD/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Security.Permissions;
+
+namespace RBD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread mainListenerThread = new Thread(MainListener.getInstance().listen);
+            mainListenerThread.Start();
+            Thread udpListenerThread = new Thread(UdpListener.getInstance().listen);
+            udpListenerThread.Start();
+            UdpSender udpSender = UdpSenderImpl.getInstance();
+            SqlConnector sqlConnector = SqlConnectorImpl.getInstance();
+            sqlConnector.query("SELECT * FROM blah");
+            SqlParser sqlParser = new SqlParserImpl();
+            ((SqlParserImpl)sqlParser).test();
+            while(true)
+            {
+                udpSender.sendToAll(new Message());
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
