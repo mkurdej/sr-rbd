fc53bb7705bf367b3b076e27c5043a2ef33bdc8e
marek.kurdej <marek.kurdej@752c7794-dde4-11de-849f-b362a63f070a>
2010-01-18 10:55:16 +0000

    C# : work in progress

---
+++ trunk/C#/RBD/RBD/Msg/Client/SuccessMessage.cs
@@ -0,0 +1,36 @@
+﻿// + TODO check
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using RBD.Communication;
+
+namespace RBD.Msg.Client
+{
+    public class SuccessMessage : ClientResponse
+    {
+        public String DatabaseMessage { get; set; }
+
+        override
+        public void FromBinary(DataInputStream s) //throws IOException 
+        {
+            DatabaseMessage = s.ReadString();
+        }
+
+        override
+        public void ToBinary(DataOutputStream s) //throws IOException 
+        {
+            s.WriteString(DatabaseMessage);
+
+        }
+
+        override
+        public MessageType GetMessageType()
+        {
+            return MessageType.CLIENT_SUCCESS;
+        }
+
+    }
+}
