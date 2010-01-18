8dbc77b04dcaa4a470b39c6f83f45fae9863949f
marek.kurdej <marek.kurdej@752c7794-dde4-11de-849f-b362a63f070a>
2010-01-18 12:32:11 +0000

    C# : work in progress

---
+++ C#/RBD/RBD/TPC/TimeoutGenerator.cs
@@ -0,0 +1,48 @@
+﻿// +-- TODO
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RBD.TPC
+{
+    public class TimeoutGenerator {
+	private volatile bool stopped = false;
+	private TimeoutListener timeoutListener;
+
+    //private Timer timer = new Timer();	
+	
+	public void setTimeoutListener(TimeoutListener timeoutListener) {
+		this.timeoutListener = timeoutListener;
+	}
+
+	/** 
+	 * <!-- begin-UML-doc -->
+	 * <!-- end-UML-doc -->
+	 * @param miliseconds
+	 * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+    public void startTimer(long miliseconds) {
+    //    timer.schedule(new TimerTask() {
+
+    //        override
+    //        public void run() {
+    //            if(!stopped) {
+    //                timeoutListener.onTimeout();
+    //            }
+    //        }
+			
+    //    }, miliseconds);
+    }
+
+	/** 
+	 * <!-- begin-UML-doc -->
+	 * <!-- end-UML-doc -->
+	 * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void stopTimer() {
+		stopped = true;
+	}
+}
+}
